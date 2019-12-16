4590d4accbada7bd012600935601f1475b821d5e
Madhumita <madhumita.shet@compassites.net>
2019-12-16 12:45:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import data from "./data";
+const Items = data.user;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class  App extends Component {
+  render(){
+    return (
+      <div className="App">
+      {/* items to display */}
+      <h2>Dashboard Page</h2>
+       <div className="blockItemsDisplay">
+        {Items && (
+                    <table>
+                    <tbody>
+                    <tr>
+                      <th>Id</th>
+                      <th>Name</th>
+                      <th>Age</th>
+                      <th>Gender</th>
+                      <th>Email</th>
+                      <th>PhoneNumber</th>
+                    </tr>
+                  {Items.map(function(item, key) {
+                      return (
+                      <tr key = {key}>
+                          <td>{item.id}</td>
+                          <td>{item.name}</td>
+                          <td>{item.age}</td>
+                          <td>{item.gender}</td>
+                          <td>{item.email}</td>
+                          <td>{item.phoneNo}</td>
+                      </tr>
+                    )
+                  })}
+                  </tbody>
+                </table>
+              )}
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
